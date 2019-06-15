cda3bacfb309593ecea6ed863c2eb4e1d76398a9
zzmartinj@gmail.com <zzmartinj@gmail.com>
2019-06-15 10:49:26 -0400

    Added first end points

--- wodit_service.py
+++ wodit_service.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, request, Response
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Test'
+    return 'Jeremy is the best!'
+
+@app.route("/workouts", methods=['GET'])
+def returnWorkouts():
+    workouts=open('./workouts.json')
+    workoutlist=json.load(workouts)    
+    workouts.close()
+    return json.dumps(workoutlist)
+
+@app.route("/workouts", methods=['POST'])
+def addWorkout():    
+    workout=json.loads(request.args['workout'])
+    workoutFile=open('./workouts.json','r')
+    workoutlist=json.load(workoutFile)
+    workoutFile.close()
+    workoutFile=open('./workouts.json','w')
+    workoutlist['workouts'].append(workout)
+    workoutFile.write(json.dumps(workoutlist))
+    workoutFile.close()
+    return Response("Processed", status=201, mimetype='application/json')
+    
 
